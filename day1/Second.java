5af1cf65eeff249862ff29eec5191f0073ea7aa9
Basantchaudashi <113439469+Basantchaudashi@users.noreply.github.com>
2022-09-14 21:28:09 +0530

    Delete day1 directory

--- day1/Second.java
+++
@@ -1,9 +0,0 @@
-class Second
-{
-public static void main(String args[])
-{
-  int i=10,j=20,k;
-      k=i+j;
-     System.out.println("k="+k);
-}
-}
--- day1/Third.java
+++
@@ -1,9 +0,0 @@
-class Third
-{
-public static void main(String args[])
-   {
-    int i =2,j=5,k;
-	k=i*j;
-	System.out.println("k="+    k);
-	}
-  }
